5a6cbe41c0201b0863f7cea4bba86b18e9cebf3b
Kofi Adu <kadu@deerfield.edu>
2016-02-10 22:13:02 -0500

    Re-organize files

---
+++ src/CharacId.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class CharacId
+{
+
+	public static void main(String[] args)
+	{
+		CharacId obj = new CharacId();
+		obj.run();
+
+	  }
+
+	  public void run() {
+
+		String csvFile = "entityfacts.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+
+			br = new BufferedReader(new FileReader("entityfacts.csv"));
+			while ((line = br.readLine()) != null) {
+
+			        // use comma as separator
+				String[] country = line.split(cvsSplitBy);
+				System.out.println(line);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("Done");
+	  }
+
+	
+
+	}
+
+
